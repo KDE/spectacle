KDE/spectacle

commit e4c2e564a5b91497132d9a20d8f521af405286bd

Replace KScreen by QScreen for current window grab
libkscreen is overkill for querying purposes, rely on QScreen to find
the current screen under cursor.

REVIEW: 129127

src/PlatformBackends/X11ImageGrabber.cpp
@@ -38,17 +38,13 @@
 
 #include <KWindowSystem>
 #include <KWindowInfo>
-#include <KScreen/Config>
-#include <KScreen/GetConfigOperation>
-#include <KScreen/Output>
 
 #include <xcb/xcb_cursor.h>
 #include <xcb/xcb_util.h>
 #include <xcb/xfixes.h>
 
 X11ImageGrabber::X11ImageGrabber(QObject *parent) :
-    ImageGrabber(parent),
-    mScreenConfigOperation(nullptr)
+    ImageGrabber(parent)
 {
     mNativeEventFilter = new OnClickEventFilter(this);
 }
@@ -85,7 +81,6 @@ bool OnClickEventFilter::nativeEventFilter(const QByteArray &eventType, void *me
 
             {
                 xcb_button_release_event_t *ev2 = static_cast<xcb_button_release_event_t *>(message);
-                qDebug() << ev2->detail;
                 if (ev2->detail == 1) {
                     QMetaObject::invokeMethod(mImageGrabber, "doImageGrab", Qt::QueuedConnection);
                 } else if (ev2->detail < 4) {
@@ -349,53 +344,6 @@ void X11ImageGrabber::KWinDBusScreenshotHelper(quint64 window)
     emit pixmapChanged(mPixmap);
 }
 
-void X11ImageGrabber::KScreenCurrentMonitorScreenshotHelper(KScreen::ConfigOperation *op)
-{
-    KScreen::ConfigPtr config = qobject_cast<KScreen::GetConfigOperation *>(op)->config();
-
-    if (!config)           { return grabFullScreen(); }
-    if (!config->screen()) { return grabFullScreen(); }
-
-    // we'll store the cursor position first
-
-    QPoint cursorPosition = QCursor::pos();
-
-    // next, we'll get all our outputs and figure out which one has the cursor
-
-    const KScreen::OutputList outputs = config->outputs();
-    for (auto output: outputs) {
-        if (!(output->isConnected())) { continue; }
-        if (!(output->currentMode())) { continue; }
-
-        QPoint screenPosition = output->pos();
-        QSize  screenSize     = output->currentMode()->size();
-        QRect  screenRect     = QRect(screenPosition, screenSize);
-
-        if (!(screenRect.contains(cursorPosition))) {
-            continue;
-        }
-
-        // bingo, we've found an output that contains the cursor. Now
-        // to take a shot
-
-        mPixmap = getWindowPixmap(QX11Info::appRootWindow(), mCapturePointer);
-        mPixmap = mPixmap.copy(screenPosition.x(), screenPosition.y(), screenSize.width(), screenSize.height());
-        emit pixmapChanged(mPixmap);
-
-        mScreenConfigOperation->disconnect();
-        mScreenConfigOperation->deleteLater();
-        mScreenConfigOperation = nullptr;
-
-        return;
-    }
-
-    mScreenConfigOperation->disconnect();
-    mScreenConfigOperation->deleteLater();
-    mScreenConfigOperation = nullptr;
-
-    return grabFullScreen();
-}
-
 void X11ImageGrabber::rectangleSelectionCancelled()
 {
     QObject *sender = QObject::sender();
@@ -621,9 +569,20 @@ QRect X11ImageGrabber::getApplicationWindowGeometry(xcb_window_t window)
 
 void X11ImageGrabber::grabCurrentScreen()
 {
-    mScreenConfigOperation = new KScreen::GetConfigOperation;
-    connect(mScreenConfigOperation, &KScreen::GetConfigOperation::finished,
-            this, &X11ImageGrabber::KScreenCurrentMonitorScreenshotHelper);
+    QPoint cursorPosition = QCursor::pos();
+    for (auto screen : QGuiApplication::screens()) {
+        const QRect screenRect = screen->geometry();
+        if (!screenRect.contains(cursorPosition)) {
+            continue;
+        }
+
+        mPixmap = getWindowPixmap(QX11Info::appRootWindow(), mCapturePointer).copy(screenRect);
+        emit pixmapChanged(mPixmap);
+        return;
+    }
+
+    // No screen found with our cursor, fallback to capturing full screen
+    grabFullScreen();
 }
 
 void X11ImageGrabber::grabRectangularRegion()

src/PlatformBackends/X11ImageGrabber.h
@@ -28,11 +28,6 @@
 #include "ImageGrabber.h"
 
 class X11ImageGrabber;
-namespace KScreen
-{
-    class GetConfigOperation;
-    class ConfigOperation;
-}
 
 class OnClickEventFilter : public QAbstractNativeEventFilter
 {
@@ -70,7 +65,6 @@ class X11ImageGrabber : public ImageGrabber
     private slots:
 
     void KWinDBusScreenshotHelper(quint64 window);
-    void KScreenCurrentMonitorScreenshotHelper(KScreen::ConfigOperation *op);
     void rectangleSelectionConfirmed(const QPixmap &pixmap, const QRect &region);
     void rectangleSelectionCancelled();
 
@@ -90,7 +84,6 @@ class X11ImageGrabber : public ImageGrabber
     QPixmap              convertFromNative(xcb_image_t *xcbImage);
 
     OnClickEventFilter          *mNativeEventFilter;
-    KScreen::GetConfigOperation *mScreenConfigOperation;
 };
 
 template <typename T> using CScopedPointer = QScopedPointer<T, QScopedPointerPodDeleter>;

CMakeLists.txt
@@ -75,7 +75,6 @@ find_package(XCB COMPONENTS XFIXES IMAGE UTIL CURSOR)
 set(XCB_COMPONENTS_ERRORS FALSE)
 if (XCB_FOUND)
 	find_package(Qt5X11Extras ${QT_MIN_VERSION} REQUIRED)
-	find_package(KF5Screen ${PLASMA_MIN_VERSION} REQUIRED)
 endif()
 set(XCB_COMPONENTS_FOUND TRUE)
 if(NOT XCB_XFIXES_FOUND)

src/CMakeLists.txt
@@ -83,7 +83,6 @@ if(XCB_FOUND)
             XCB::CURSOR
             XCB::UTIL
             Qt5::X11Extras
-            KF5::Screen
     )
 endif()
 

commit a41e83edbaadd27cd8ea85d4dfa375033e8ade84

Fix blurry preview on HiDPI displays
THe captured pixmap should always have devicePixelRatio one, it only
needs to be changed when it is actually drawn in the preview widget.

Also fixes a regression from e4c2e564a5b91497132d9a20d8f521af405286bd
("Replace KScreen by QScreen for current window grab"), now the current
screen grab mode will again capture the full screen on HiDPI screens.

Tested with: QT_SCALE_FACTOR=2 src/spectacle

REVIEW: 129189

src/Gui/KSImageWidget.cpp
@@ -43,7 +43,10 @@ void KSImageWidget::setScreenshot(const QPixmap &pixmap)
 
 void KSImageWidget::setScaledPixmap()
 {
-    setPixmap(mPixmap.scaled(size() * mPixmap.devicePixelRatio(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    const qreal scale = qApp->devicePixelRatio();
+    QPixmap scaledPixmap = mPixmap.scaled(size() * scale, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    scaledPixmap.setDevicePixelRatio(scale);
+    setPixmap(scaledPixmap);
 }
 
 // drag handlers

src/PlatformBackends/X11ImageGrabber.cpp
@@ -220,17 +220,13 @@ QPixmap X11ImageGrabber::convertFromNative(xcb_image_t *xcbImage)
 
 // utility functions
 
+// Note: x, y, width and height are measured in device pixels
 QPixmap X11ImageGrabber::blendCursorImage(const QPixmap &pixmap, int x, int y, int width, int height)
 {
-    // first we get the cursor position, compute the co-ordinates of the region
-    // of the screen we're grabbing, and see if the cursor is actually visible in
-    // the region
+    // If the cursor position lies outside the area, do not bother drawing a cursor.
 
-    const qreal dpr = pixmap.devicePixelRatio();
-
-    // cursor position operates on application's device pixel ratio, not the pixmap!
-    QPoint cursorPos = QCursor::pos() / dpr * qApp->devicePixelRatio();
-    QRect screenRect(x / dpr, y / dpr, width / dpr, height / dpr);
+    QPoint cursorPos = getNativeCursorPosition();
+    QRect screenRect(x, y, width, height);
 
     if (!screenRect.contains(cursorPos)) {
         return pixmap;
@@ -254,15 +250,14 @@ QPixmap X11ImageGrabber::blendCursorImage(const QPixmap &pixmap, int x, int y, i
     // process the image into a QImage
 
     QImage cursorImage = QImage((quint8 *)pixelData, cursorReply->width, cursorReply->height, QImage::Format_ARGB32_Premultiplied);
-    cursorImage.setDevicePixelRatio(dpr);
 
     // a small fix for the cursor position for fancier cursors
 
-    cursorPos -= QPoint(cursorReply->xhot, cursorReply->yhot) / dpr;
+    cursorPos -= QPoint(cursorReply->xhot, cursorReply->yhot);
 
     // now we translate the cursor point to our screen rectangle
 
-    cursorPos -= QPoint(x, y) / dpr;
+    cursorPos -= QPoint(x, y);
 
     // and do the painting
 
@@ -434,7 +429,6 @@ void X11ImageGrabber::grabTransientWithParent()
 
     // now that we know we have a transient window, let's
     // find other possible transient windows and the app window itself.
-
     QRegion clipRegion;
 
     QSet<xcb_window_t> transients;
@@ -613,12 +607,14 @@ void X11ImageGrabber::grabCurrentScreen()
 {
     QPoint cursorPosition = QCursor::pos();
     for (auto screen : QGuiApplication::screens()) {
-        const QRect screenRect = screen->geometry();
+        QRect screenRect = screen->geometry();
         if (!screenRect.contains(cursorPosition)) {
             continue;
         }
 
-        mPixmap = getToplevelPixmap(screenRect, mCapturePointer);
+        // The screen origin is in native pixels, but the size is device-dependent. Convert these also to native pixels.
+        QRect nativeScreenRect(screenRect.topLeft(), screenRect.size() * screen->devicePixelRatio());
+        mPixmap = getToplevelPixmap(nativeScreenRect, mCapturePointer);
         emit pixmapChanged(mPixmap);
         return;
     }
@@ -700,4 +696,17 @@ xcb_window_t X11ImageGrabber::getTransientWindowParent(xcb_window_t winId, QRect
         outRect = winInfo.geometry();
     }
     return winInfo.transientFor();
+}
+
+QPoint X11ImageGrabber::getNativeCursorPosition()
+{
+    // QCursor::pos() is not used because it requires additional calculations.
+    // Its value is the offset to the origin of the current screen is in
+    // device-independent pixels while the origin itself uses native pixels.
+
+    xcb_connection_t *xcbConn = QX11Info::connection();
+    xcb_query_pointer_cookie_t pointerCookie = xcb_query_pointer_unchecked(xcbConn, QX11Info::appRootWindow());
+    CScopedPointer<xcb_query_pointer_reply_t> pointerReply(xcb_query_pointer_reply(xcbConn, pointerCookie, NULL));
+
+    return QPoint(pointerReply->root_x, pointerReply->root_y);
 }

src/PlatformBackends/X11ImageGrabber.h
@@ -84,6 +84,7 @@ class X11ImageGrabber : public ImageGrabber
     QPixmap              getWindowPixmap(xcb_window_t window, bool blendPointer);
     QPixmap              convertFromNative(xcb_image_t *xcbImage);
     xcb_window_t         getTransientWindowParent(xcb_window_t winId, QRect &outRect);
+    QPoint               getNativeCursorPosition();
 
     OnClickEventFilter          *mNativeEventFilter;
 };