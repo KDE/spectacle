KDE/spectacle

commit 63bc26c6656595660d25e9309a07659fc0326021

Refactor saveMode handling to use enums instead of ints
Summary:
Things like `case 0` make it really hard to work with the code and
having access to the current state of the {nav Save} button is needed
for a subsequent patch.

Test Plan: Spectacle still remembers the last used save state, defaults work too.

Reviewers: #spectacle, ngraham

Reviewed By: #spectacle, ngraham

Subscribers: ngraham

Differential Revision: https://phabricator.kde.org/D10467

src/SpectacleConfig.cpp
@@ -197,14 +197,14 @@ void SpectacleConfig::setUseLightRegionMaskColour(bool enabled)
 
 // last used save mode
 
-int SpectacleConfig::lastUsedSaveMode() const
+SaveMode SpectacleConfig::lastUsedSaveMode() const
 {
-    return mGuiConfig.readEntry(QStringLiteral("lastUsedSaveMode"), 0);
+    return static_cast<SaveMode>(mGuiConfig.readEntry(QStringLiteral("lastUsedSaveMode"), 0));
 }
 
-void SpectacleConfig::setLastUsedSaveMode(int index)
+void SpectacleConfig::setLastUsedSaveMode(SaveMode mode)
 {
-    mGuiConfig.writeEntry(QStringLiteral("lastUsedSaveMode"), index);
+    mGuiConfig.writeEntry(QStringLiteral("lastUsedSaveMode"), static_cast<int>(mode));
     mGuiConfig.sync();
 }
 

src/SpectacleConfig.h
@@ -27,6 +27,11 @@
 #include <KSharedConfig>
 #include <KConfigGroup>
 
+enum SaveMode {
+    SaveAs,
+    Save
+};
+
 class SpectacleConfig : public QObject
 {
     Q_OBJECT
@@ -85,8 +90,8 @@ class SpectacleConfig : public QObject
     bool useLightRegionMaskColour() const;
     void setUseLightRegionMaskColour(bool enabled);
 
-    int lastUsedSaveMode() const;
-    void setLastUsedSaveMode(int index);
+    SaveMode lastUsedSaveMode() const;
+    void setLastUsedSaveMode(SaveMode mode);
 
     QString autoSaveFilenameFormat() const;
     void setAutoSaveFilenameFormat(const QString &format);

src/Gui/KSMainWindow.cpp
@@ -106,6 +106,12 @@ KSMainWindow::KSMainWindow(bool onClickAvailable, QWidget *parent) :
 KSMainWindow::~KSMainWindow()
 {}
 
+SaveMode KSMainWindow::saveButtonMode() const
+{
+    const SpectacleConfig *cfgManager = SpectacleConfig::instance();
+    return cfgManager->useDynamicSaveButton() ? cfgManager->lastUsedSaveMode() : SaveMode::SaveAs;
+}
+
 // GUI init
 
 void KSMainWindow::init()
@@ -225,18 +231,13 @@ void KSMainWindow::buildSaveMenu()
     QAction *actionSave = KStandardAction::save(this, &KSMainWindow::save, this);
     QAction *actionSaveAs = KStandardAction::saveAs(this, &KSMainWindow::saveAs, this);
 
-    // static or dynamic
-    SpectacleConfig *cfgManager = SpectacleConfig::instance();
-    int switchState = cfgManager->useDynamicSaveButton() ? cfgManager->lastUsedSaveMode() : 0;
-
     // put the actions in order
-    switch (switchState) {
-    case 0:
-    default:
+    switch (saveButtonMode()) {
+    case SaveMode::SaveAs:
         mSaveButton->setDefaultAction(actionSaveAs);
         mSaveMenu->addAction(actionSave);
         break;
-    case 1:
+    case SaveMode::Save:
         mSaveButton->setDefaultAction(actionSave);
         mSaveMenu->addAction(actionSaveAs);
         break;
@@ -339,7 +340,7 @@ void KSMainWindow::setScreenshotWindowTitle(QUrl location)
 
 void KSMainWindow::save()
 {
-    SpectacleConfig::instance()->setLastUsedSaveMode(1);
+    SpectacleConfig::instance()->setLastUsedSaveMode(SaveMode::Save);
     buildSaveMenu();
 
     if (SpectacleConfig::instance()->quitAfterSaveOrCopyChecked()) {
@@ -354,7 +355,7 @@ void KSMainWindow::save()
 
 void KSMainWindow::saveAs()
 {
-    SpectacleConfig::instance()->setLastUsedSaveMode(0);
+    SpectacleConfig::instance()->setLastUsedSaveMode(SaveMode::SaveAs);
     buildSaveMenu();
 
     if (SpectacleConfig::instance()->quitAfterSaveOrCopyChecked()) {

src/Gui/KSMainWindow.h
@@ -31,6 +31,7 @@
 #include "PlatformBackends/ImageGrabber.h"
 #include "ExportMenu.h"
 #include "KSWidget.h"
+#include "SpectacleConfig.h"
 
 class KSMainWindow : public QDialog
 {
@@ -40,6 +41,10 @@ class KSMainWindow : public QDialog
 
     explicit KSMainWindow(bool onClickAvailable, QWidget *parent = 0);
     ~KSMainWindow();
+
+    private:
+
+    SaveMode saveButtonMode() const;
 
     private slots:
 

commit b6a185b2b39763afc77d79bc7c614b4281d54597

Prevent occasional empty Save button when upgrading from earlier version
Summary:
63bc26c66565 introduced a regression, where when upgrading from
Spectacle 17.12 with {nav Save As} used last to 18.04 the {nav Save}
button would show no text at all and clicking would be a no-op (the down
arrow menu still worked, though).

The reason is the removal of a `default` case, however the root problem
has been there before: `lastUsedSaveMode()` could pass invalid values.
This can be fixed by properly refactoring the latter method too and
trading meticulosity for compactness when converting from `int` to
`enum`, i.e. adding the `default` to the correct place. If Spectacle
used KConfigXT this would not have been a problem in the first place…

Test Plan:
- `rm ~/.config/spectaclerc`
- Start Spectacle 17.12, {nav Save As}, {nav Quit}
- Start Spectacle from master
{nav Save} button now correctly shows the default text instead of
nothing at all. Button state still remembered after restart.

Reviewers: #spectacle, ngraham

Reviewed By: #spectacle, ngraham

Subscribers: ngraham

Differential Revision: https://phabricator.kde.org/D10682

src/SpectacleConfig.cpp
@@ -199,7 +199,13 @@ void SpectacleConfig::setUseLightRegionMaskColour(bool enabled)
 
 SaveMode SpectacleConfig::lastUsedSaveMode() const
 {
-    return static_cast<SaveMode>(mGuiConfig.readEntry(QStringLiteral("lastUsedSaveMode"), 0));
+    switch (mGuiConfig.readEntry(QStringLiteral("lastUsedSaveMode"), 0)) {
+        default:
+        case 0:
+            return SaveMode::SaveAs;
+        case 1:
+            return SaveMode::Save;
+    }
 }
 
 void SpectacleConfig::setLastUsedSaveMode(SaveMode mode)

src/SpectacleConfig.h
@@ -27,7 +27,7 @@
 #include <KSharedConfig>
 #include <KConfigGroup>
 
-enum SaveMode {
+enum class SaveMode {
     SaveAs,
     Save
 };