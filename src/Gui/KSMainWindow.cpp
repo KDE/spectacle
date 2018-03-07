KDE/spectacle

commit 3b69ac3a7aff4dedd4a2578174f5465f02fcc02b

Optimize #include
Test Plan: If it compiles, all is fine.

Reviewers: #spectacle, rkflx

Reviewed By: #spectacle, rkflx

Subscribers: rkflx, #spectacle

Differential Revision: https://phabricator.kde.org/D11084

src/Gui/KSMainWindow.cpp
@@ -18,35 +18,30 @@
  */
 
 #include "KSMainWindow.h"
+
 #include "Config.h"
+#include "SettingsDialog/SettingsDialog.h"
+
+#include <KAboutData>
+#include <KGuiItem>
+#include <KHelpMenu>
+#include <KIO/OpenFileManagerWindowJob>
+#include <KLocalizedString>
+#include <KNS3/KMoreToolsMenuFactory>
+#include <KStandardAction>
+#include <KStandardGuiItem>
+#include <KWindowSystem>
 
 #include <QDesktopServices>
 #include <QJsonArray>
 #include <QPrintDialog>
-#include <QShortcut>
-#include <QTimer>
 #include <QPushButton>
+#include <QTimer>
 #include <QVBoxLayout>
-
 #ifdef XCB_FOUND
 #include <QX11Info>
-#include <xcb/xcb.h>
 #endif
 
-#include <KLocalizedString>
-#include <KGuiItem>
-#include <KStandardAction>
-#include <KStandardGuiItem>
-#include <KHelpMenu>
-#include <KAboutData>
-#include <KWindowSystem>
-#include <KIO/OpenFileManagerWindowJob>
-#include <KNS3/KMoreToolsMenuFactory>
-
-#include "SettingsDialog/SettingsDialog.h"
-#include "ExportMenu.h"
-#include "ExportManager.h"
-#include "SpectacleConfig.h"
 
 static const int DEFAULT_WINDOW_HEIGHT = 420;
 static const int DEFAULT_WINDOW_WIDTH = 840;

src/Gui/ExportMenu.cpp
@@ -19,25 +19,19 @@
 
 #include "ExportMenu.h"
 
-#include <QTimer>
-#include <QList>
-#include <QUrl>
-#include <QJsonObject>
-#include <QJsonArray>
-#include <QDebug>
+#include "Config.h"
 
 #include <KLocalizedString>
-#include <KStandardShortcut>
-#include <KService>
 #include <KMimeTypeTrader>
 #include <KRun>
-
-#include "Config.h"
+#include <KStandardShortcut>
 #ifdef KIPI_FOUND
 #include <KIPI/Plugin>
-#include <KIPI/PluginLoader>
-#include "KipiInterface/KSGKipiInterface.h"
 #endif
+
+#include <QDebug>
+#include <QJsonArray>
+#include <QTimer>
 
 ExportMenu::ExportMenu(QWidget *parent) :
     QMenu(parent),

src/Gui/KSWidget.cpp
@@ -19,23 +19,21 @@
 
 #include "KSWidget.h"
 
-#include <QGridLayout>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
-#include <QFormLayout>
-#include <QComboBox>
+#include "KSImageWidget.h"
+#include "SmartSpinBox.h"
+#include "SpectacleConfig.h"
+
+#include <KLocalizedString>
+
 #include <QCheckBox>
+#include <QComboBox>
+#include <QFormLayout>
+#include <QGridLayout>
 #include <QLabel>
 #include <QPushButton>
 #include <QShortcut>
 #include <QTimer>
 
-#include <KLocalizedString>
-
-#include "KSImageWidget.h"
-#include "SmartSpinBox.h"
-
-#include "SpectacleConfig.h"
 
 KSWidget::KSWidget(QWidget *parent) :
     QWidget(parent)

src/Gui/SettingsDialog/GeneralOptionsPage.cpp
@@ -19,14 +19,13 @@
 
 #include "GeneralOptionsPage.h"
 
-#include <QGroupBox>
-#include <QVBoxLayout>
-#include <QLabel>
-#include <QCheckBox>
+#include "SpectacleConfig.h"
 
 #include <KLocalizedString>
 
-#include "SpectacleConfig.h"
+#include <QCheckBox>
+#include <QGroupBox>
+#include <QVBoxLayout>
 
 GeneralOptionsPage::GeneralOptionsPage(QWidget *parent) :
     SettingsPage(parent)

src/Gui/SettingsDialog/SaveOptionsPage.cpp
@@ -19,19 +19,17 @@
 
 #include "SaveOptionsPage.h"
 
+#include "SpectacleConfig.h"
+
+#include <KIOWidgets/KUrlRequester>
+#include <KLocalizedString>
+
 #include <QLineEdit>
 #include <QLabel>
 #include <QGroupBox>
 #include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QStandardPaths>
 #include <QComboBox>
 #include <QImageWriter>
-
-#include <KLocalizedString>
-#include <KIOWidgets/KUrlRequester>
-
-#include "SpectacleConfig.h"
 
 SaveOptionsPage::SaveOptionsPage(QWidget *parent) :
     SettingsPage(parent)

src/Gui/SettingsDialog/SettingsDialog.cpp
@@ -19,15 +19,13 @@
 
 #include "SettingsDialog.h"
 
-#include <QMetaObject>
-#include <QIcon>
-#include <QMessageBox>
+#include "GeneralOptionsPage.h"
+#include "SaveOptionsPage.h"
 
 #include <KLocalizedString>
-#include <KPageWidgetModel>
 
-#include "SaveOptionsPage.h"
-#include "GeneralOptionsPage.h"
+#include <QIcon>
+#include <QMessageBox>
 
 SettingsDialog::SettingsDialog(QWidget *parent) :
     KPageDialog(parent)

src/Main.cpp
@@ -17,22 +17,16 @@
  *  Boston, MA 02110-1301, USA.
  */
 
-#include <QApplication>
-#include <QIcon>
-#include <QObject>
-#include <QString>
-#include <QCommandLineParser>
-#include <QDBusConnection>
-#include <QDebug>
-
-#include <KAboutData>
-#include <KLocalizedString>
-#include <KDBusService>
-
+#include "Config.h"
 #include "SpectacleCore.h"
 #include "SpectacleDBusAdapter.h"
-#include "ExportManager.h"
-#include "Config.h"
+
+#include <KAboutData>
+#include <KDBusService>
+#include <KLocalizedString>
+
+#include <QCommandLineParser>
+#include <QDBusConnection>
 
 int main(int argc, char **argv)
 {

src/PlatformBackends/KWinWaylandImageGrabber.cpp
@@ -22,12 +22,11 @@
 #include <QDBusPendingCall>
 #include <QDBusPendingReply>
 #include <QDBusUnixFileDescriptor>
-
-#include <QtConcurrentRun>
 #include <QFutureWatcher>
+#include <QtConcurrentRun>
 #include <qplatformdefs.h>
 
-#include <errno.h>
+#include <cerrno>
 
 static int readData(int fd, QByteArray &data)
 {

commit 941c1a517be82bed25d1254ebd735c29b0d2951c

Fix build
Summary:
3b69ac3a7aff broke the build on the CI:
  06:57:49 [ 42%] Building CXX object src/CMakeFiles/spectacle.dir/Gui/KSWidget.cpp.o
  06:57:50 /usr/home/jenkins/workspace/Applications spectacle kf5-qt5 FreeBSDQt5.9/src/Gui/KSMainWindow.cpp:91:9: error: unknown type name 'xcb_intern_atom_cookie_t'

BUG: 391504

Reviewers: rkflx, #spectacle

Reviewed By: rkflx, #spectacle

Subscribers: rkflx

Differential Revision: https://phabricator.kde.org/D11110

src/Gui/KSMainWindow.cpp
@@ -40,6 +40,7 @@
 #include <QVBoxLayout>
 #ifdef XCB_FOUND
 #include <QX11Info>
+#include <xcb/xcb.h>
 #endif
 
 