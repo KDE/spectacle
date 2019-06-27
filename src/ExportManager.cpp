KDE/spectacle

commit 0a1f081ebe75eaf25a1142e5efd00cb2354e35fe

Change defaultSaveLocation to use QUrl
Summary:
There were two issues with the default save lcoation. First the url passed to
QFileDialog was missing the "file://" prefix and the file dialog would open in
the working directory of the application (usually /). The second issue hidden
by this was that the last part of the path (~/Pictures) was removed because it
was returned through functions which expect file paths and trim them to get a
path to a folder. To avoid this a slash is added to the path.

BUG: 406058

Test Plan:
Open Spectacle with a fresh user or delete lines related to save paths from
your spectaclerc. Clicking on "Save as..." should open the dialog in
~/Pictures.

Reviewers: #spectacle, ngraham

Reviewed By: #spectacle, ngraham

Subscribers: ngraham, broulik

Tags: #spectacle

Differential Revision: https://phabricator.kde.org/D21556

src/ExportManager.cpp
@@ -122,16 +122,12 @@ void ExportManager::setTimestamp(const QDateTime &timestamp)
 
 QString ExportManager::defaultSaveLocation() const
 {
-    QString savePath = SpectacleConfig::instance()->defaultSaveLocation();
-    if (savePath.isEmpty() || savePath.isNull()) {
-        savePath = QDir::homePath();
-    }
+    QString savePath = SpectacleConfig::instance()->defaultSaveLocation().toString();
     savePath = QDir::cleanPath(savePath);
 
     QDir savePathDir(savePath);
     if (!(savePathDir.exists())) {
         savePathDir.mkpath(QStringLiteral("."));
-        SpectacleConfig::instance()->setDefaultSaveLocation(savePath);
     }
 
     return savePath;

src/Gui/KSMainWindow.cpp
@@ -313,7 +313,7 @@ void KSMainWindow::openScreenshotsFolder()
     case SaveMode::Save:
         location = cfgManager->lastSaveFile();
         if (!exportManager->isFileExists(location)) {
-            location = QUrl(cfgManager->defaultSaveLocation());
+            location = cfgManager->defaultSaveLocation();
         }
         break;
     case SaveMode::SaveAs:

src/Gui/SettingsDialog/SaveOptionsPage.cpp
@@ -163,7 +163,7 @@ void SaveOptionsPage::saveChanges()
 
     // save the data
 
-    cfgManager->setDefaultSaveLocation(mUrlRequester->url().toDisplayString(QUrl::PreferLocalFile));
+    cfgManager->setDefaultSaveLocation(mUrlRequester->url());
     cfgManager->setAutoSaveFilenameFormat(mSaveNameFormat->text());
     cfgManager->setSaveImageFormat(mSaveImageFormat->currentText().toLower());
     cfgManager->setCopySaveLocationToClipboard(mCopyPathToClipboard->checkState() == Qt::Checked);
@@ -183,7 +183,7 @@ void SaveOptionsPage::resetChanges()
     // read in the data
 
     mSaveNameFormat->setText(cfgManager->autoSaveFilenameFormat());
-    mUrlRequester->setUrl(QUrl::fromUserInput(cfgManager->defaultSaveLocation()));
+    mUrlRequester->setUrl(cfgManager->defaultSaveLocation());
     mCopyPathToClipboard->setChecked(cfgManager->copySaveLocationToClipboard());
     mQualitySlider->setSliderPosition(cfgManager->compressionQuality());
 

src/SpectacleConfig.cpp
@@ -53,8 +53,7 @@ QString SpectacleConfig::defaultTimestampTemplate() const
 
 QUrl SpectacleConfig::lastSaveAsFile() const
 {
-    return mGeneralConfig.readEntry(QStringLiteral("lastSaveAsFile"),
-                                    QUrl(this->defaultSaveLocation()));
+    return mGeneralConfig.readEntry(QStringLiteral("lastSaveAsFile"), this->defaultSaveLocation());
 }
 
 void SpectacleConfig::setLastSaveAsFile(const QUrl &location)
@@ -72,8 +71,7 @@ QUrl SpectacleConfig::lastSaveAsLocation() const
 
 QUrl SpectacleConfig::lastSaveFile() const
 {
-    return mGeneralConfig.readEntry(QStringLiteral("lastSaveFile"),
-                                    QUrl(this->defaultSaveLocation()));
+    return mGeneralConfig.readEntry(QStringLiteral("lastSaveFile"), this->defaultSaveLocation());
 }
 
 void SpectacleConfig::setLastSaveFile(const QUrl &location)
@@ -305,15 +303,19 @@ void SpectacleConfig::setAutoSaveFilenameFormat(const QString &format)
 
 // autosave location
 
-QString SpectacleConfig::defaultSaveLocation() const
+QUrl SpectacleConfig::defaultSaveLocation() const
 {
-    return mGeneralConfig.readPathEntry(QStringLiteral("default-save-location"),
-                          QStandardPaths::writableLocation(QStandardPaths::PicturesLocation));
+    QString path = mGeneralConfig.readPathEntry(QStringLiteral("default-save-location"),
+        (QStandardPaths::writableLocation(QStandardPaths::PicturesLocation)));
+    if (! path.endsWith(QLatin1Char('/'))) {
+        path += QLatin1Char('/');
+    }
+    return QUrl::fromUserInput(path);
 }
 
-void SpectacleConfig::setDefaultSaveLocation(const QString &location)
+void SpectacleConfig::setDefaultSaveLocation(const QUrl &location)
 {
-    mGeneralConfig.writePathEntry(QStringLiteral("default-save-location"), location);
+    mGeneralConfig.writePathEntry(QStringLiteral("default-save-location"), location.toString());
     mGeneralConfig.sync();
 }
 

src/SpectacleConfig.h
@@ -120,8 +120,8 @@ class SpectacleConfig : public QObject
     QString autoSaveFilenameFormat() const;
     void setAutoSaveFilenameFormat(const QString &format);
 
-    QString defaultSaveLocation() const;
-    void setDefaultSaveLocation(const QString &location);
+    QUrl defaultSaveLocation() const;
+    void setDefaultSaveLocation(const QUrl &location);
 
     bool copySaveLocationToClipboard() const;
     void setCopySaveLocationToClipboard(bool enabled);

commit 4fb409bfb12d90f8e7636b59c3d8c1190d8bfcfa

Fix counting up placeholder (%Nd)
Summary: The change from QString to Qurl in 0a1f081ebe75eaf25a1142e5efd00cb2354e35fe broke it.

Test Plan: Press save button two times

Reviewers: #spectacle, broulik

Reviewed By: broulik

Subscribers: broulik

Tags: #spectacle

Differential Revision: https://phabricator.kde.org/D22113

src/ExportManager.cpp
@@ -122,7 +122,7 @@ void ExportManager::setTimestamp(const QDateTime &timestamp)
 
 QString ExportManager::defaultSaveLocation() const
 {
-    QString savePath = SpectacleConfig::instance()->defaultSaveLocation().toString();
+    QString savePath = SpectacleConfig::instance()->defaultSaveLocation().toLocalFile();
     savePath = QDir::cleanPath(savePath);
 
     QDir savePathDir(savePath);