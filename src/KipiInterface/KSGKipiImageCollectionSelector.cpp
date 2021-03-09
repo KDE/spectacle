222b4190d50a1ad8c5d089ae084aa7dadda01852
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-03-09 20:56:01 +0100

    Convert license headers to SPDX

--- src/KipiInterface/KSGKipiImageCollectionSelector.cpp
+++ src/KipiInterface/KSGKipiImageCollectionSelector.cpp
@@ -1,22 +1,10 @@
 /*
- *  Copyright (C) 2015 Boudhayan Gupta <me@BaloneyGeek.com>
- *  Copyright 2010 Pau Garcia i Quiles <pgquiles@elpauer.org>
+ *  SPDX-FileCopyrightText: 2015 Boudhayan Gupta <me@BaloneyGeek.com>
+ *  SPDX-FileCopyrightText: 2010 Pau Garcia i Quiles <pgquiles@elpauer.org>
  *  based on code for Gwenview by
- *  Copyright 2008 Aurélien Gâteau <agateau@kde.org>
+ *  SPDX-FileCopyrightText: 2008 Aurélien Gâteau <agateau@kde.org>
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.
+ *  SPDX-License-Identifier: LGPL-2.0-or-later
  *
 */
 
--- src/KipiInterface/KSGKipiImageCollectionShared.h
+++ src/KipiInterface/KSGKipiImageCollectionShared.h
@@ -1,24 +1,11 @@
 /*
- *  Copyright (C) 2015 Boudhayan Gupta <me@BaloneyGeek.com>
- *  Copyright (C) 2010 Pau Garcia i Quiles <pgquiles@elpauer.org>
+ *  SPDX-FileCopyrightText: 2015 Boudhayan Gupta <me@BaloneyGeek.com>
+ *  SPDX-FileCopyrightText: 2010 Pau Garcia i Quiles <pgquiles@elpauer.org>
  *  Essentially a rip-off of code for Kamoso by:
- *  Copyright (C) 2008-2009 by Aleix Pol <aleixpol@kde.org>
- *  Copyright (C) 2008-2009 by Alex Fiestas <alex@eyeos.org>
+ *  SPDX-FileCopyrightText: 2008-2009 Aleix Pol <aleixpol@kde.org>
+ *  SPDX-FileCopyrightText: 2008-2009 Alex Fiestas <alex@eyeos.org>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA 02110-1301, USA.
+ *  SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #ifndef KSGKIPIIMAGECOLLECTIONSHARED_H
