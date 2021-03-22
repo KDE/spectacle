1243e0e08d263fb44f8434d1c8c74be2e687d245
Srevin Saju <srevinsaju@sugarlabs.org>
2021-03-22 20:16:36 +0300

    feat: use radio buttons for after screenshot clipboard actions
    Move UI elements, and remove unnecessary Save location
    radiobutton code
    
    fix: disable 'copy to clipboard' checkbox when 'copy file location' is also selected
    
    spectacle previously allowed copying both the image as well as file
    location simultaneously to the clipboard. This MR also attempts to fix a corner case which
    requires 'Autosave image' to be enabled to 'copy file location to clipboard'.
    Previously, if 'Autosave Image' checkbox was not enabled, it was still possible for the user
    to copy the file location to clipboard. However, this had indirectly triggered an autosave first.
    This is confusing, as spectacle saves the file to its default save location without it
    being asked to. So it would be ideal to disable 'Copy file location' if 'Auto Save' option was
    not enabled in the first place
    
    fix: typo in label name

--- src/Gui/SettingsDialog/GeneralOptionsPage.cpp
+++ src/Gui/SettingsDialog/GeneralOptionsPage.cpp
@@ -23,9 +23,6 @@ GeneralOptionsPage::GeneralOptionsPage(QWidget *parent)
     m_ui->runningTitle->setLevel(2);
     m_ui->regionTitle->setLevel(2);
 
-    connect(m_ui->kcfg_copyImageToClipboard, &QCheckBox::stateChanged, this, &GeneralOptionsPage::updateAutomaticActions);
-    connect(m_ui->kcfg_autoSaveImage, &QCheckBox::stateChanged, this, &GeneralOptionsPage::updateAutomaticActions);
-
     //On Wayland  we can't programmatically raise and focus the window so we have to hide the option
     if (KWindowSystem::isPlatformWayland() || qstrcmp(qgetenv("XDG_SESSION_TYPE").data(), "wayland") == 0) {
        delete m_ui->activateWindowButton;
@@ -33,14 +30,3 @@ GeneralOptionsPage::GeneralOptionsPage(QWidget *parent)
 }
 
 GeneralOptionsPage::~GeneralOptionsPage() = default;
-
-void GeneralOptionsPage::updateAutomaticActions() {
-    if (m_ui->kcfg_copyImageToClipboard->isChecked() && m_ui->kcfg_autoSaveImage->isChecked()) {
-        m_ui->kcfg_copySaveLocation->setCheckState(Qt::CheckState::Unchecked);
-        m_ui->kcfg_copySaveLocation->setCheckable(false);
-        m_ui->kcfg_copySaveLocation->setDisabled(true);
-    } else {
-        m_ui->kcfg_copySaveLocation->setCheckable(true);
-        m_ui->kcfg_copySaveLocation->setEnabled(true);
-    }
-}
