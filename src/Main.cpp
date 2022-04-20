0a720d36ec104f7e7eed0c2def1532f583a6df51
Nicolas Fella <nicolas.fella@gmx.de>
2022-04-20 23:51:07 +0200

    QGuiApplication::setFallbackSessionManagementEnabled is gone in Qt6

--- src/Main.cpp
+++ src/Main.cpp
@@ -59,7 +59,9 @@ int main(int argc, char **argv)
     lCmdLineParser.process(app.arguments());
     aboutData.processCommandLine(&lCmdLineParser);
 
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QGuiApplication::setFallbackSessionManagementEnabled(false);
+#endif
 
     auto disableSessionManagement = [](QSessionManager &sm) {
         sm.setRestartHint(QSessionManager::RestartNever);
