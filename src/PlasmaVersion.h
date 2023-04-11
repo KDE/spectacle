3f852574190dc17653c3a166fe60dea27c8eba1f
Noah Davis <noahadvs@gmail.com>
2023-04-11 18:44:16 -0400

    Add licenses to PlasmaVersion

--- src/PlasmaVersion.h
+++ src/PlasmaVersion.h
@@ -1,3 +1,7 @@
+/* SPDX-FileCopyrightText: 2023 Noah Davis <noahadvs@gmail.com>
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
 #include <QDBusConnection>
 #include <QDBusMessage>
 #include <QDBusVariant>
