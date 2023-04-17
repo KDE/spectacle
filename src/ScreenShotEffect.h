6e5a3f392324e5ae310709225540d37448539569
Noah Davis <noahadvs@gmail.com>
2023-04-17 17:52:27 -0400

    Warn when screenshot effect isn't available on wayland, exit if no GUI
    CCBUG: 451842

---
+++ src/ScreenShotEffect.h
@@ -0,0 +1,20 @@
+/* SPDX-FileCopyrightText: 2023 Noah Davis <noahadvs@gmail.com>
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include <QtGlobal>
+
+class ScreenShotEffect
+{
+public:
+    static bool isLoaded();
+    static quint32 version();
+
+private:
+    ScreenShotEffect() = delete;
+    ~ScreenShotEffect() = delete;
+    ScreenShotEffect(const ScreenShotEffect &) = delete;
+    ScreenShotEffect(ScreenShotEffect &&) = delete;
+    ScreenShotEffect &operator=(const ScreenShotEffect &) = delete;
+    ScreenShotEffect &operator=(ScreenShotEffect &&) = delete;
+};
