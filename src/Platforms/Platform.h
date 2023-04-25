4a66af78a896f9f556e93c2961593f893a2c38ee
Noah Davis <noahadvs@gmail.com>
2023-04-25 14:50:00 -0400

    Platform: use more consistent variable names

--- src/Platforms/Platform.h
+++ src/Platforms/Platform.h
@@ -53,7 +53,7 @@ Q_SIGNALS:
     void newScreensScreenshotTaken(const QVector<CanvasImage> &screenImages);
 
     void newScreenshotFailed();
-    void windowTitleChanged(const QString &theWindowTitle = {});
+    void windowTitleChanged(const QString &windowTitle = {});
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(Platform::GrabModes)
