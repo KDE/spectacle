KDE/spectacle

commit a14cd1a41cc25e34c1f68af8e436e6cd3798f27d

Make the settings bar for the screenshot take at least 1/3 of the window
Since we added the recording options, it is weird that it resizes depending
on the one that is shown. Instead just own the size, offer the implicit
content size in case it is still not fitting (because weird languages or so,
still should seldom be the case).

src/Gui/ImageView.qml
@@ -271,6 +271,7 @@ EmptyPage {
             bottom: parent.bottom
             right: parent.right
         }
+        width: Math.min(parent.width/3, Kirigami.Units.gridUnit * 15)
         sourceComponent: QQC2.Page {
 
             leftPadding: Kirigami.Units.mediumSpacing * 2
@@ -311,9 +312,6 @@ EmptyPage {
                     }
                 }
             }
-
-            onImplicitContentWidthChanged: contentWidth = Math.max(contentWidth, implicitContentWidth)
-            onImplicitContentHeightChanged: contentHeight = Math.max(contentHeight, implicitContentHeight)
         }
     }
 

commit 9c2aa586a0ba012cbf389253f309c015b08dd846

Fix the capture option panel width for some languages
For some languages like french (longer words), it doesn't fit at all.
Regression introduced with : a14cd1a41cc25e34c1f68af8e436e6cd3798f27d

src/Gui/ImageView.qml
@@ -129,7 +129,7 @@ EmptyPage {
             bottom: parent.bottom
             right: parent.right
         }
-        width: Math.min(parent.width/3, Kirigami.Units.gridUnit * 15)
+        width: Math.max(implicitWidth, Kirigami.Units.gridUnit * 15)
         sourceComponent: QQC2.Page {
 
             leftPadding: Kirigami.Units.mediumSpacing * 2