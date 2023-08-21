7ab0aaf16042558971a641933c66b8f407fa658d
Noah Davis <noahadvs@gmail.com>
2023-08-21 16:39:34 -0400

    Only use ScreenShot2 on X11 when Plasma is at least version 5.27.8
    That's the version where an infinite loop in KWin was fixed.

--- src/Platforms/PlatformLoader.cpp
+++ src/Platforms/PlatformLoader.cpp
@@ -6,6 +6,7 @@
 
 #include "PlatformLoader.h"
 #include "Config.h"
+#include "PlasmaVersion.h"
 #include "ScreenShotEffect.h"
 
 #include "PlatformKWin.h"
@@ -21,11 +22,15 @@
 PlatformPtr loadPlatform()
 {
     PlatformPtr platform;
-    if (ScreenShotEffect::isLoaded()) {
+    // Check XDG_SESSION_TYPE because Spectacle might be using the XCB platform via XWayland
+    const bool isReallyX11 = KWindowSystem::isPlatformX11() && qstrcmp(qgetenv("XDG_SESSION_TYPE").constData(), "wayland") != 0;
+    // Before KWin 5.27.8, there was an infinite loop in KWin on X11 when doing rectangle captures.
+    // Spectacle uses CaptureScreen DBus calls to KWin for rectangle captures.
+    if (ScreenShotEffect::isLoaded() && (!isReallyX11 || PlasmaVersion::get() >= PlasmaVersion::check(5, 27, 8))) {
         platform = PlatformKWin::create();
     }
 #ifdef XCB_FOUND
-    else if (KWindowSystem::isPlatformX11() && qstrcmp(qgetenv("XDG_SESSION_TYPE").constData(), "wayland") != 0) {
+    else if (isReallyX11) {
         platform = std::make_unique<PlatformXcb>();
     }
 #endif
