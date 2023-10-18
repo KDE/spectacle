12b5d6b8bc14ad61520f7083366ef2c31f56d6b1
Noah Davis <noahadvs@gmail.com>
2023-10-18 08:31:27 -0400

    Use u""_s for single char CLI options
    Using u'' caused the flags to stop working.

--- src/CommandLineOptions.h
+++ src/CommandLineOptions.h
@@ -18,85 +18,85 @@ struct CommandLineOptions {
     // i18n() can't be used in static code,
     // so we can't just make the variables static and use them directly.
     const QCommandLineOption fullscreen = {
-        {u'f', u"fullscreen"_s},
+        {u"f"_s, u"fullscreen"_s},
         i18n("Capture the entire desktop (default)")
     };
     const QCommandLineOption current = {
-        {u'm', u"current"_s},
+        {u"m"_s, u"current"_s},
         i18n("Capture the current monitor")
     };
     const QCommandLineOption activeWindow = {
-        {u'a', u"activewindow"_s},
+        {u"a"_s, u"activewindow"_s},
         i18n("Capture the active window")
     };
     const QCommandLineOption windowUnderCursor = {
-        {u'u', u"windowundercursor"_s},
+        {u"u"_s, u"windowundercursor"_s},
         i18n("Capture the window currently under the cursor, including parents of pop-up menus")
     };
     const QCommandLineOption transientOnly = {
-        {u't', u"transientonly"_s},
+        {u"t"_s, u"transientonly"_s},
         i18n("Capture the window currently under the cursor, excluding parents of pop-up menus")
     };
     const QCommandLineOption region = {
-        {u'r', u"region"_s},
+        {u"r"_s, u"region"_s},
         i18n("Capture a rectangular region of the screen")
     };
     const QCommandLineOption launchOnly = {
-        {u'l', u"launchonly"_s},
+        {u"l"_s, u"launchonly"_s},
         i18n("Launch Spectacle without taking a screenshot")
     };
     const QCommandLineOption gui = {
-        {u'g', u"gui"_s},
+        {u"g"_s, u"gui"_s},
         i18n("Start in GUI mode (default)")
     };
     const QCommandLineOption background = {
-        {u'b', u"background"_s},
+        {u"b"_s, u"background"_s},
         i18n("Take a screenshot and exit without showing the GUI")
     };
     const QCommandLineOption dbus = {
-        {u's', u"dbus"_s},
+        {u"s"_s, u"dbus"_s},
         i18n("Start in DBus-Activation mode")
     };
     const QCommandLineOption noNotify = {
-        {u'n', u"nonotify"_s},
+        {u"n"_s, u"nonotify"_s},
         i18n("In background mode, do not pop up a notification when the screenshot is taken")
     };
     const QCommandLineOption output = {
-        {u'o', u"output"_s},
+        {u"o"_s, u"output"_s},
         i18n("In background mode, save image to specified file"),
         u"fileName"_s
     };
     const QCommandLineOption delay = {
-        {u'd', u"delay"_s},
+        {u"d"_s, u"delay"_s},
         i18n("In background mode, delay before taking the shot (in milliseconds)"),
         u"delayMsec"_s
     };
     const QCommandLineOption copyImage = {
-        {u'c', u"copy-image"_s},
+        {u"c"_s, u"copy-image"_s},
         i18n("In background mode, copy screenshot image to clipboard, unless -o is also used.")
     };
     const QCommandLineOption copyPath = {
-        {u'C', u"copy-path"_s},
+        {u"C"_s, u"copy-path"_s},
         i18n("In background mode, copy screenshot file path to clipboard")
     };
     const QCommandLineOption onClick = {
-        {u'w', u"onclick"_s},
+        {u"w"_s, u"onclick"_s},
         i18n("Wait for a click before taking screenshot. Invalidates delay")
     };
     const QCommandLineOption newInstance = {
-        {u'i', u"new-instance"_s},
+        {u"i"_s, u"new-instance"_s},
         i18n("Starts a new GUI instance of spectacle without registering to DBus")
     };
     const QCommandLineOption pointer = {
-        {u'p', u"pointer"_s},
+        {u"p"_s, u"pointer"_s},
         i18n("In background mode, include pointer in the screenshot")
     };
     const QCommandLineOption noDecoration = {
-        {u'e', u"no-decoration"_s},
+        {u"e"_s, u"no-decoration"_s},
         i18n("In background mode, exclude decorations in the screenshot")
     };
     const QCommandLineOption editExisting = {
-        {u'E', u"edit-existing"_s},
+        {u"E"_s, u"edit-existing"_s},
         i18n("Open and edit existing screenshot file"),
         u"existingFileName"_s
     };
