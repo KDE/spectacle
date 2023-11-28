b889a9474060ce46c07edca81bf09189ab00f690
Noah Davis <noahadvs@gmail.com>
2023-11-28 15:37:54 -0500

    Localize default filename template settings
    BUG: 477583

---
+++ kconf_update/spectacle-24.02.0-keep_old_filename_templates.cpp
@@ -0,0 +1,33 @@
+/* SPDX-FileCopyrightText: 2023 Noah Davis <noahadvs@gmail.com>
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "ConfigUtils.h"
+#include <KConfigGroup>
+#include <KSharedConfig>
+
+using namespace Qt::StringLiterals;
+
+int main()
+{
+    const auto fileName = u"spectaclerc"_s;
+    if (!continueUpdate(fileName, u"2024-02-28T00:00:00Z"_s)) {
+        return 0;
+    }
+
+    // We only need to read spectaclerc, so we use SimpleConfig.
+    auto spectaclerc = KSharedConfig::openConfig(fileName, KConfig::SimpleConfig);
+
+    // Preserve old defaults for existing users that didn't already have these set.
+    auto imageSaveGroup = spectaclerc->group(QStringLiteral("ImageSave"));
+    if (isEntryDefault(imageSaveGroup, "imageFilenameTemplate")) {
+        imageSaveGroup.writeEntry("imageFilenameTemplate", "Screenshot_%Y%M%D_%H%m%S");
+    }
+
+    auto videoSaveGroup = spectaclerc->group(QStringLiteral("VideoSave"));
+    if (isEntryDefault(videoSaveGroup, "videoFilenameTemplate")) {
+        videoSaveGroup.writeEntry("videoFilenameTemplate", "Screencast_%Y%M%D_%H%m%S");
+    }
+
+    return spectaclerc->sync() ? 0 : 1;
+}
