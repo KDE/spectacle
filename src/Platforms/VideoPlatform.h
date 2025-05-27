71c6fd79c2afb0ddb19fc915ae8902d33b8de364
Noah Davis <noahadvs@gmail.com>
2025-05-27 17:22:58 -0400

    Fix AnimatedImage warning about failing to load non-image formats
    (cherry picked from commit 650e94c81b391a84be312a7527e9f7dd38ccf38e)
    
    Co-authored-by: Noah Davis <noahadvs@gmail.com>

--- src/Platforms/VideoPlatform.h
+++ src/Platforms/VideoPlatform.h
@@ -122,7 +122,7 @@ public:
     static QString extensionForFormat(Format format);
 
     static Format formatForExtension(const QString &extension);
-    static Format formatForPath(const QString &path);
+    Q_INVOKABLE static Format formatForPath(const QString &path);
 
     bool isRecording() const;
     qint64 recordedTime() const;
