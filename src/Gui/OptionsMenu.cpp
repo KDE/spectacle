KDE/spectacle

commit e5f1a6ef499d4569db8dc2ddd0a282caa6cf7c60

Fix crash on quit with a quickly selected region
If you quickly selected a region and quickly pressed escape to quit, it could cause a crash.

BUG: 517064

FIXED-IN: 6.6.3


(cherry picked from commit 3dff870b1628c29a0bbbab0fa44ebd0515769524)

Co-authored-by: Noah Davis <noahadvs@gmail.com>

src/Gui/OptionsMenu.cpp
@@ -23,7 +23,7 @@
 
 using namespace Qt::StringLiterals;
 
-static std::unique_ptr<OptionsMenu> s_instance = nullptr;
+static OptionsMenu *s_instance = nullptr;
 
 OptionsMenu::OptionsMenu(QWidget *parent)
     : SpectacleMenu(parent)
@@ -152,16 +152,25 @@ OptionsMenu::OptionsMenu(QWidget *parent)
             });
 }
 
+OptionsMenu::~OptionsMenu()
+{
+    s_instance = nullptr;
+}
+
 OptionsMenu *OptionsMenu::instance()
 {
     if (!s_instance && SpectacleCore::instance()) {
-        s_instance = std::unique_ptr<OptionsMenu>(new OptionsMenu);
-        // We have to destroy this after SpectacleCore to prevent a crash from the Qt Quick UI.
-        connect(SpectacleCore::instance(), &QObject::destroyed, s_instance.get(), [] {
-            s_instance.reset();
+        s_instance = new OptionsMenu;
+        // We have to destroy this after SpectacleCore to ensure that destructors
+        // are called. We don't just rely on smart pointers because they won't delete
+        // the menus at the right time and cause a crash while quitting.
+        connect(SpectacleCore::instance(), &QObject::destroyed, qApp, [] {
+            if (s_instance) {
+                delete s_instance;
+            }
         });
     }
-    return s_instance.get();
+    return s_instance;
 }
 
 void OptionsMenu::showPreferencesDialog()

src/Gui/OptionsMenu.h
@@ -42,6 +42,7 @@ protected:
     void mouseReleaseEvent(QMouseEvent *event) override;
 
     explicit OptionsMenu(QWidget *parent = nullptr);
+    ~OptionsMenu();
 
     void delayActionLayoutUpdate();
     const std::unique_ptr<QWidgetAction> m_delayAction;

src/Gui/ExportMenu.cpp
@@ -33,7 +33,7 @@
 using namespace std::chrono_literals;
 using namespace Qt::StringLiterals;
 
-static std::unique_ptr<ExportMenu> s_instance = nullptr;
+static ExportMenu *s_instance = nullptr;
 
 ExportMenu::ExportMenu(QWidget *parent)
     : SpectacleMenu(parent)
@@ -60,16 +60,25 @@ ExportMenu::ExportMenu(QWidget *parent)
     getKServiceItems();
 }
 
+ExportMenu::~ExportMenu()
+{
+    s_instance = nullptr;
+}
+
 ExportMenu *ExportMenu::instance()
 {
     if (!s_instance && SpectacleCore::instance()) {
-        s_instance = std::unique_ptr<ExportMenu>(new ExportMenu);
-        // We have to destroy this after SpectacleCore to prevent a crash from the Qt Quick UI.
-        connect(SpectacleCore::instance(), &QObject::destroyed, s_instance.get(), [] {
-            s_instance.reset();
+        s_instance = new ExportMenu;
+        // We have to destroy this after SpectacleCore to ensure that destructors
+        // are called. We don't just rely on smart pointers because they won't delete
+        // the menus at the right time and cause a crash while quitting.
+        connect(SpectacleCore::instance(), &QObject::destroyed, qApp, [] {
+            if (s_instance) {
+                delete s_instance;
+            }
         });
     }
-    return s_instance.get();
+    return s_instance;
 }
 
 void ExportMenu::onImageChanged()

src/Gui/ExportMenu.h
@@ -47,6 +47,7 @@ Q_SIGNALS:
 
 private:
     explicit ExportMenu(QWidget *parent = nullptr);
+    ~ExportMenu();
 
     Q_SLOT void onImageChanged();
     Q_SLOT void openScreenshotsFolder();

src/Gui/HelpMenu.cpp
@@ -17,7 +17,7 @@
 
 using namespace Qt::StringLiterals;
 
-static std::unique_ptr<HelpMenu> s_instance = nullptr;
+static HelpMenu *s_instance = nullptr;
 
 static QObject *findWidgetOfType(const char *className)
 {
@@ -43,16 +43,25 @@ HelpMenu::HelpMenu(QWidget* parent)
     connect(this, &QMenu::triggered, this, &HelpMenu::onTriggered);
 }
 
+HelpMenu::~HelpMenu()
+{
+    s_instance = nullptr;
+}
+
 HelpMenu *HelpMenu::instance()
 {
     if (!s_instance && SpectacleCore::instance()) {
-        s_instance = std::unique_ptr<HelpMenu>(new HelpMenu);
-        // We have to destroy this after SpectacleCore to prevent a crash from the Qt Quick UI.
-        connect(SpectacleCore::instance(), &QObject::destroyed, s_instance.get(), [] {
-            s_instance.reset();
+        s_instance = new HelpMenu;
+        // We have to destroy this after SpectacleCore to ensure that destructors
+        // are called. We don't just rely on smart pointers because they won't delete
+        // the menus at the right time and cause a crash while quitting.
+        connect(SpectacleCore::instance(), &QObject::destroyed, qApp, [] {
+            if (s_instance) {
+                delete s_instance;
+            }
         });
     }
-    return s_instance.get();
+    return s_instance;
 }
 
 void HelpMenu::showAppHelp()

src/Gui/HelpMenu.h
@@ -34,6 +34,7 @@ public:
 
 private:
     explicit HelpMenu(QWidget *parent = nullptr);
+    ~HelpMenu();
     Q_SLOT void onTriggered(QAction *action);
     const std::unique_ptr<KHelpMenu> kHelpMenu;
     friend class HelpMenuSingleton;

src/Gui/RecordingModeMenu.cpp
@@ -10,7 +10,7 @@
 
 using namespace Qt::StringLiterals;
 
-static std::unique_ptr<RecordingModeMenu> s_instance = nullptr;
+static RecordingModeMenu *s_instance = nullptr;
 
 RecordingModeMenu::RecordingModeMenu(QWidget *parent)
     : SpectacleMenu(i18nc("@title:menu", "Recording Modes"), parent)
@@ -54,16 +54,25 @@ RecordingModeMenu::RecordingModeMenu(QWidget *parent)
     connect(RecordingModeModel::instance(), &RecordingModeModel::recordingModesChanged, this, addModes);
 }
 
+RecordingModeMenu::~RecordingModeMenu()
+{
+    s_instance = nullptr;
+}
+
 RecordingModeMenu *RecordingModeMenu::instance()
 {
     if (!s_instance && SpectacleCore::instance()) {
-        s_instance = std::unique_ptr<RecordingModeMenu>(new RecordingModeMenu);
-        // We have to destroy this after SpectacleCore to prevent a crash from the Qt Quick UI.
-        connect(SpectacleCore::instance(), &QObject::destroyed, s_instance.get(), [] {
-            s_instance.reset();
+        s_instance = new RecordingModeMenu;
+        // We have to destroy this after SpectacleCore to ensure that destructors
+        // are called. We don't just rely on smart pointers because they won't delete
+        // the menus at the right time and cause a crash while quitting.
+        connect(SpectacleCore::instance(), &QObject::destroyed, qApp, [] {
+            if (s_instance) {
+                delete s_instance;
+            }
         });
     }
-    return s_instance.get();
+    return s_instance;
 }
 
 #include "moc_RecordingModeMenu.cpp"

src/Gui/RecordingModeMenu.h
@@ -27,4 +27,5 @@ public:
 
 private:
     explicit RecordingModeMenu(QWidget *parent = nullptr);
+    ~RecordingModeMenu();
 };

commit 0649bb56e6d12c5c22a1b41ebc11c4e5530615be

OptionsMenu: Drop double ownership of delay widgets
OptionsMenu wraps its QWidgetAction (m_delayAction), the action's default
widget (m_delayWidget), and the widget's children in std::unique_ptr
members. That double-books ownership with Qt: QWidgetAction documents that
it takes ownership of its default widget and deletes it in ~QWidgetAction,
and QMenu caches the action/widget pair in a QHash<QAction *, QWidget *>
(widgetItems) that ~QMenu() iterates via raw pointers.

This is a latent use-after-free. If OptionsMenu is ever destroyed after
QCoreApplication is gone (so QCoreApplication::sendEvent returns false and
the ActionRemoved event that cleans widgetItems is never delivered), the
unique_ptrs free the action and widget during member destruction, and then
~QMenu() calls releaseWidget() on the dangling widgetItems entries.

Commit c905a768 ("Destroy Qt Widget menu singletons after SpectacleCore is
destroyed") exposed this by making OptionsMenu's singleton a static
std::unique_ptr whose destructor fired during exit finalization, after
QApplication was already torn down. Commit e5f1a6ef ("Fix crash on quit
with a quickly selected region") later switched to a raw-pointer singleton
with a qApp-context lambda to fix a different crash. As a side effect, the
lambda is now disconnected before it fires, so OptionsMenu is silently
leaked instead of deleted. The leak masks the double-ownership bug but does
not fix it.

Fix by dropping the unique_ptr wrappers and relying on Qt's documented
ownership chain:

- m_delayAction is a child of OptionsMenu (Qt parent-child), so ~QObject
  deletes it.
- m_delayWidget is the default widget of m_delayAction, so ~QWidgetAction
  deletes it.
- m_delayLayout, m_delayLabel, and m_delaySpinBox are children of
  m_delayWidget and are deleted with their parent.

This also makes OptionsMenu safe to delete at any time, so the singleton
leak introduced by e5f1a6ef can be fixed separately without reintroducing
the crash.

src/Gui/OptionsMenu.cpp
@@ -29,9 +29,9 @@ OptionsMenu::OptionsMenu(QWidget *parent)
     : SpectacleMenu(parent)
     , m_delayAction(new QWidgetAction(this))
     , m_delayWidget(new QWidget(this))
-    , m_delayLayout(new QHBoxLayout(m_delayWidget.get()))
-    , m_delayLabel(new QLabel(m_delayWidget.get()))
-    , m_delaySpinBox(new SmartSpinBox(m_delayWidget.get()))
+    , m_delayLayout(new QHBoxLayout(m_delayWidget))
+    , m_delayLabel(new QLabel(m_delayWidget))
+    , m_delaySpinBox(new SmartSpinBox(m_delayWidget))
 {
     setToolTipsVisible(true);
     // QMenu::addSection just adds an action with text and separator mode enabled
@@ -104,8 +104,8 @@ OptionsMenu::OptionsMenu(QWidget *parent)
     }
 
     // set up delay widget
-    auto spinbox = m_delaySpinBox.get();
-    auto label = m_delayLabel.get();
+    auto spinbox = m_delaySpinBox;
+    auto label = m_delayLabel;
     label->setText(i18nc("@label:spinbox", "Delay:"));
     spinbox->setDecimals(1);
     spinbox->setSingleStep(1.0);
@@ -122,13 +122,13 @@ OptionsMenu::OptionsMenu(QWidget *parent)
     QObject::connect(Settings::self(), &Settings::captureDelayChanged, spinbox, [this](){
         m_delaySpinBox->setValue(Settings::captureDelay());
     });
-    m_delayWidget->setLayout(m_delayLayout.get());
+    m_delayWidget->setLayout(m_delayLayout);
     m_delayLayout->addWidget(label);
     m_delayLayout->addWidget(spinbox);
     m_delayLayout->setAlignment(Qt::AlignLeft);
-    m_delayAction->setDefaultWidget(m_delayWidget.get());
+    m_delayAction->setDefaultWidget(m_delayWidget);
     m_delayAction->setEnabled(!hasOnClick || !Settings::captureOnClick());
-    addAction(m_delayAction.get());
+    addAction(m_delayAction);
 
     addSection(i18nc("@title:menu", "Recording Settings"));
 

src/Gui/OptionsMenu.h
@@ -45,11 +45,11 @@ protected:
     ~OptionsMenu();
 
     void delayActionLayoutUpdate();
-    const std::unique_ptr<QWidgetAction> m_delayAction;
-    const std::unique_ptr<QWidget> m_delayWidget;
-    const std::unique_ptr<QHBoxLayout> m_delayLayout;
-    const std::unique_ptr<QLabel> m_delayLabel;
-    const std::unique_ptr<SmartSpinBox> m_delaySpinBox;
+    QWidgetAction *const m_delayAction;
+    QWidget *const m_delayWidget;
+    QHBoxLayout *const m_delayLayout;
+    QLabel *const m_delayLabel;
+    SmartSpinBox *const m_delaySpinBox;
     bool m_updatingDelayActionLayout = false;
 };
 