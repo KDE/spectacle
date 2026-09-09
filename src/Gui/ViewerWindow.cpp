900272984cfcde7f84ad0ac0ba88f1dc88b926af
Tobias Fella <tobias.fella@kde.org>
2026-09-09 10:49:06 +0200

    Remove message shown after image is shared
    The message isn't great in a few ways:
    - (Depending on settings) it clutters up the top area in combination with other messages shown
    - The text isn't great by itself
    - In the current form, the text is also broken because the url is missing
    - It's wrong, as sharing generally isn't to the clipboard
    - Improving it is hard, as we'd need to be very generic ("Image shared successfully"), feedback should be shown by the share backends as it makes sense
    
    (cherry picked from commit 0b1bc263cb3bdcd77765c5dd1ba40628a79d036d)

--- src/Gui/ViewerWindow.cpp
+++ src/Gui/ViewerWindow.cpp
@@ -52,12 +52,6 @@ ViewerWindow::ViewerWindow(Mode mode, QQmlEngine *engine, QWindow *parent)
         if (error) {
             auto text = i18nc("@info, %1 is the error message", "There was a problem sharing the image: %1", message);
             InlineMessageModel::instance()->push(InlineMessageModel::Error, text);
-        } else {
-            auto text = i18nc("@info", "The shared image link (<a href=\"%1\">%1</a>) has been copied to the clipboard.", message);
-            InlineMessageModel::instance()->push(InlineMessageModel::Shared, text);
-            if (!message.isEmpty()) {
-                QApplication::clipboard()->setText(message);
-            }
         }
     });
 
